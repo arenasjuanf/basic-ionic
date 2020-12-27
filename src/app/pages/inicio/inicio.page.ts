a63c4ed79420de7e8b9377436a1f6d463781301a
Juanf <juanfa107@gmail.com>
2020-12-27 12:36:49 -0500

    se agrega list component

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  selector: "app-inicio",
+  templateUrl: "./inicio.page.html",
+  styleUrls: ["./inicio.page.scss"],
 })
 export class InicioPage implements OnInit {
+  componentes: componente[] = [
+    {
+      icon: "american-football",
+      name: "Action Sheet",
+      route: "/action-sheet",
+    },
+    {
+      icon: "analytics-outline",
+      name: "Alert",
+      route: "/alert",
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+}
 
-  ngOnInit() {
-  }
-
+interface componente {
+  icon: string;
+  name: string;
+  route: string;
 }
